f52fa5610e4e3afedbf0a549854069f7aac1575c
merimirela <meri.vanguelova@gmail.com>
2025-08-08 18:01:25 +0100

    Update ft_atoi.c

--- exam2/ex2/ft_atoi.c
+++ exam2/ex2/ft_atoi.c
@@ -26,7 +26,7 @@ int	ft_atoi(const char *str)
 	sign = 1;
 	result = 0;
 	
-	if (str[i] == 0 || (str[i] >= 9 && str[i] <= 13))
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
@@ -34,7 +34,10 @@ int	ft_atoi(const char *str)
 			sign = -sign;
 		i++;
 	}
-		
-
-
+	while (str[i] >= '0' && str[i] <= '9')
+		{
+			result = result * 10 + (str[i] - '0');
+			i++;
+		}
+	return (sign * result);
 }
